132bd1b83f3b2900c59c784863ab0bfefeddee43
Hiya Jayswal <87514873+hiya1801@users.noreply.github.com>
2026-04-02 13:15:36 -0400

    Add initial DOM element selections in script.js
    Initialize script.js with DOM element selections.

---
+++ Assignment_3/js/script.js
@@ -0,0 +1,8 @@
+// Select DOM Elements
+const taskInput = document.getElementById('taskInput');       // Input box
+const addTaskBtn = document.getElementById('addTaskBtn');     // Add button
+const taskList = document.getElementById('taskList');         // UL for tasks
+const taskCounter = document.getElementById('taskCounter');   // Task counter
+const dingSound = document.getElementById('dingSound');       // Ding sound
+const deleteSound = document.getElementById('deleteSound');   // Delete sound
+const message = document.getElementById('message');           // "Task added!" message
