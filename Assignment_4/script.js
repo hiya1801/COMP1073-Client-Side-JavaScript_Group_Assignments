50f0dc0434c683679dc7488135045b9bfdbf958d
abiskar-jj <abiskarchhetri86@gmail.com>
2026-04-10 18:30:07 -0400

    Created script.js and added main DOM selectors

---
+++ Assignment_4/script.js
@@ -0,0 +1,13 @@
+// Store your OpenWeather API key here
+const apiKey = "b3bd4d943bce5e136bd399de785ee63c";
+
+// Get references to important elements on the page
+const cityInput = document.getElementById("cityInput");
+const searchBtn = document.getElementById("searchBtn");
+const message = document.getElementById("message");
+
+const weatherCard = document.getElementById("weatherCard");
+const countryCard = document.getElementById("countryCard");
+
+const weatherContent = document.getElementById("weatherContent");
+const countryContent = document.getElementById("countryContent");
